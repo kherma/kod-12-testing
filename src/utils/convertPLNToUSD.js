3907992e8bcefa27fffcdb08c2acd25876e609cf
kherma <kamil.herma01@gmail.com>
2026-01-19 19:06:08 +0100

    feat: add unit tests and fixes to convertPLNtoUSD

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,26 @@
 export const convertPLNToUSD = (PLN) => {
+  if (PLN === undefined) return NaN;
 
-  const PLNtoUSD = PLN / 3.5;
-  
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
+  if (typeof PLN === 'string') return NaN;
 
-  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
-}
+  if (PLN === null || typeof PLN === 'object' || typeof PLN === 'function') {
+    return 'Error';
+  }
+
+  if (typeof PLN === 'number' && PLN < 0) {
+    return '$0.00';
+  }
+
+  if (typeof PLN === 'number') {
+    const PLNtoUSD = PLN / 3.5;
+
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+
+    return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+  }
+
+  return 'Error';
+};
